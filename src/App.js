d44b9e15e418e8b9b5ce87f9f769016d4bc7d68b
sctlcd <lucinda.souchet@gmail.com>
2021-12-01 09:34:01 +0100

    feat: default language translations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useTranslation, Trans } from 'react-i18next';
+
 function App() {
+  const { t } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
